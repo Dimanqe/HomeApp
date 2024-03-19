bf7ab227783e38653bc3618ab73206d3e03f1ad2
Дмитрий Санкеев <d.sankeev@gmail.com>
2024-03-20 01:15:17 +0300

    Add project files.

---
+++ HomeApp/HomeApp/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeApp.Pages
+{
+    public partial class RegisterPage : ContentPage
+    {
+        // Константа для текста кнопки
+        public const string BUTTON_TEXT = "Register";
+        // Переменная счетчика
+        
+
+        public static string email = null;
+
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// По клику обрабатываем счётчик и выводим разные сообщения
+        /// </summary>
+        private void Register_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                // Если первая попытка - просто меняем сообщения
+                loginButton.Text = $"Введите email";
+            }
+           
+        }
+    }
+}
